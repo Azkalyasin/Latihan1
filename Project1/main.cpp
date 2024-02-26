403dff43cce3f562ac9eb6e4b9d5ddda67ae6a44
Azkalyasin <yasinadzkiaazkal@gmail.com>
2024-02-26 17:28:15 +0700

    perbaikan

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -2,51 +2,53 @@
 using namespace std;
 
 int main()
-{ 
+{
+	
+	while (true) {
+		int pilhan;
 
-	int pilhan;
-	char ulang;
+		cout << "------------------------------" << endl;
+		cout << " menu minuman toko kopi saputra" << endl;
+		cout << "______________________________" << endl;
 
-	cout << "------------------------------" << endl;
-	cout << " menu minuman toko kopi saputra" << endl;
-	cout << "______________________________" << endl;
+		cout << "1.Latte" << endl;
+		cout << "2.Americano" << endl;
+		cout << "3.Espresso" << endl;
+		cout << "4.Cappucinno" << endl;
+		cout << "5. Keluar " << endl;
+		cout << endl;
+		cout << "masukkan pilihan menu: " << endl;
+		cin >> pilhan;
 
-	cout << "1.Latte" << endl;
-	cout << "2.Americano" << endl;
-	cout << "3.Espresso" << endl;
-	cout << "4.Cappucinno" << endl;
-	cout << endl;
+		switch (pilhan) {
+		case 1:
+			cout << "Anda memilih Latte" << endl;
+			break;
+		case 2:
+			cout << "Anda memilih Americano" << endl;
+			break;
+		case 3:
+			cout << "Anda memilih Espresso" << endl;
+			break;
+		case 4:
+			cout << "Anda memilih Cappucinno" << endl;
+			break;
+		case 5:
+			false;
+			return 0;
+		default:
+			cout << "menu yang ada pilih tidak ada di dalam menu" << endl;
+		}
+		cout << endl;
 
-	cout << "masukkan pilihan menu: " << endl;
-	cin >> pilhan;
-
-	switch (pilhan){
-	case 1 :
-		cout << "Anda memilih Latte" << endl;
-		break;
-	case 2 :
-		cout << "Anda memilih Americano" << endl;
-		break;
-	case 3:
-		cout << "Anda memilih Espresso" << endl;
-		break;
-	case 4:
-		cout << "Anda memilih Cappucinno" << endl;
-		break;
-	default:
-		cout << "menu yang ada pilih tidak ada di dalam menu" << endl;
+		
+		
 	}
-	cout << endl;
-
-	cout << "Apakaih ingin memilih menu lain (y/t): ";
-	cin >> ulang;
-	cout << endl;
-
-	while (ulang != 't') {
-		cout << ulang<< endl;
-	}
+	
 
 	cout << "Terima kasih telah memesan" << endl;
-
 	return 0;
 }
+
+	
+
